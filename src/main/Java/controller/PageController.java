6f4a4335181b5f2dcd1651c4c02f893142775c95
IvanBylinovich <ib6020607@gmail.com>
2021-10-07 17:58:32 +0300

    controller correction

--- src/main/Java/controller/PageController.java
+++ src/main/Java/controller/PageController.java
@@ -4,27 +4,33 @@ import exception.NegativePageNumberException;
 import tool.Executor;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.logging.Logger;
 
-@Path("/reducedPageNumbers")
+@Path("/")
 
 public class PageController  {
     Executor reducer = new Executor();
+    Logger logger;
+
 
     @GET
-    @Produces("application/json")
-    @Consumes("text/plain")
-    public String doGet(@QueryParam("rawPageNumbers") String rawPageNumbers){
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response doGet(@QueryParam("rawPageNumbers") String rawPageNumbers){
         String reducedPageNumbersList = "";
+        logger.info("reducedPageNumbers GET controller run");
 
         try {
             reducedPageNumbersList = reducer.toFormat(rawPageNumbers);
         } catch (NegativePageNumberException e) {
-            return  e.getMessage();
+            return Response.status(Response.Status.BAD_REQUEST).entity("The page cannot be less than 1!").build();
+
         }catch (NumberFormatException e) {
-            return e.getMessage();
+            return Response.status(Response.Status.BAD_REQUEST).entity("A non-numeric value has been entered!").build();
         }
 
-        return "{\"original\":" + "\""+ rawPageNumbers + "\", \"reduced\": \"" + reducedPageNumbersList+ "\"}";
+        return Response.ok("{\"original\":" + "\""+ rawPageNumbers + "\", \"reduced\": \"" + reducedPageNumbersList+ "\"}").build();
 
 
     }
